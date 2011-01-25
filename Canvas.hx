b84c64761f334bdeb70a5f89854ad7195db8be02
tong <sdk@disktree.net>
2011-01-25 18:23:18 +0100

    import

---
+++ Canvas.hx
@@ -0,0 +1,8 @@
+
+typedef Canvas = {/*>js.HTMLElement,*/
+	var width : Int;
+	var height : Int;
+	function getContext( id : String ) : CanvasRenderingContext2D;
+	//DOMString toDataURL(optional in DOMString type, in any... args);
+	function toDataURL( ?mimetype : String ) : String;
+}
---
+++ Image.hx
@@ -0,0 +1,12 @@
+
+extern class Image {
+	var alt : String;
+	var src : String;
+	var useMap : String;
+	var isMap : Bool;
+	var width : Int;
+	var height : Int;
+	var complete : Bool;//readonly
+	var onload : Void->Void;
+	function new() : Void;
+}
