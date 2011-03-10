e09a58274f20e67db6abbae781b2b1d618c98efe
tong <sdk@disktree.net>
2011-03-10 13:06:49 +0100

    added (webkit) notification api

---
+++ Notification.hx
@@ -0,0 +1,8 @@
+
+extern class Notification {
+	function show() : Void;
+	function cancel() : Void;
+	var ondisplay : Void->Void;
+	var onerror : Void->Void;
+	var onclose : Void->Void;
+}
---
+++ NotificationCenter.hx
@@ -0,0 +1,7 @@
+
+@:native("window.webkitNotifications") extern class NotificationCenter {
+	static function createNotification( iconUrl : String, title : String, body : String ) : Notification;
+	static function createHTMLNotification( url : String ) : Notification;
+	static function requestPermission( ?cb : Void->Void ) : Void;
+	static function checkPermission() : Int;
+}
