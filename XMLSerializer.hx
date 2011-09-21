32d947af977421d5547c7f026cdf1193f1c46aa6
tong <sdk@disktree.net>
2011-09-21 13:49:56 +0200

    added XMLSerializer

---
+++ XMLSerializer.hx
@@ -0,0 +1,6 @@
+
+extern class XMLSerializer {
+	function new() : Void;
+	function serializeToString( d : Dynamic ) : String;
+	function serializeToStream( d : Dynamic, stream : Dynamic, encoding : String ) : Void;
+}
