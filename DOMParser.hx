37cc769f13e2f5d272bef108efe78ebaf67bc295
tong <sdk@disktree.net>
2011-09-22 12:56:25 +0200

    added DOMParser.

---
+++ DOMParser.hx
@@ -0,0 +1,8 @@
+
+extern class DOMParser {
+	function new() : Void;
+	//function parseFromBuffer(  ) : Dynamic;
+	//function parseFromStream(  ) : Dynamic;
+	function parseFromString( t : String, mime : String ) : Dynamic;
+	function getElementsByTagName( name : String ) : Dynamic;
+}
