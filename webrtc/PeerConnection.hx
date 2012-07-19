9d522b454fbafd8228fda802c2b93406753bb484
tong <tong@disktree.net>
2012-07-19 11:01:46 +0200

    updated peerconnection api

--- webrtc/PeerConnection.hx
+++ webrtc/PeerConnection.hx
@@ -1,7 +1,27 @@
 package webrtc;
 
 //TODO
-@:native("webkitDeprecatedPeerConnection") extern class PeerConnection {
+
+@:native("SessionDescription")
+extern class SessionDescription {
+	var type : SdpType;
+	var sdp : String;
+}
+
+@:native("SdpType")
+@:fakeEnum(String)
+enum SdpType {
+	offer;
+	pranswer;
+	answer;
+}
+
+typedef SessionDescriptionCallback = SessionDescription->Void;
+typedef PeerConnectionErrorCallback = String->Void;
+typedef MediaConstraints = Dynamic; //TODO
+
+@:native("webkitDeprecatedPeerConnection")
+extern class PeerConnection {
 
 	static var NEW(default,never) : Int;
 	static var NEGOTIATING(default,never) : Int;
@@ -22,18 +42,25 @@ package webrtc;
 	static var SDP_GLARE(default,never) : Int;
 	
 	static var sdpState(default,never) : Int;
-
+	
+	//var iceState(default,never) : IceState; //TODO
 	var readyState(default,never) : Int;
 	var iceState(default,never) : Int;
 
 	var localStreams(default,null) : Array<MediaStream>;
 	var remoteStreams(default,null) : Array<MediaStream>;
 	
+	var localDescription(default,null) : SessionDescription;
+	var remoteDescription(default,null) : SessionDescription;
+	
+	var onrenegotationneeded : Void->Void;
+	var onicecandidat : Void->Void;
 	var onconnecting : Void->Void;
 	var onopen : Void->Void;
 	var onstatechange : Void->Void;
 	var onaddstream : Dynamic->Void;
 	var onremovestream : Void->Void;
+	var onicechange : Void->Void;
 	
 	function new( configuration : String, signalingCallback : String->Void ) : Void; //TODO SignalingCallback
 	
@@ -41,5 +68,9 @@ package webrtc;
 	function removeStream( stream : MediaStream ) : Void;
 	function close() : Void;
 	function processSignalingMessage( message : String ) : Void;
+	
+	function createOffer( successCallback : SessionDescriptionCallback, ?failureCallback : PeerConnectionErrorCallback, ?constraints : MediaConstraints ) : Void;
+	function createAnswer( offer : SessionDescription, successCallback : SessionDescriptionCallback, ?failureCallback : PeerConnectionErrorCallback, ?constraints : MediaConstraints, ?createProvisionalAnswer : Bool ) : Void;
+	function setLocalDescription( action : SdpType, description : SessionDescription ) : Void;
 	
 }
