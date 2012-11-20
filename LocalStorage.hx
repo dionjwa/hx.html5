d1e6989d6fd6496c8411e48fdd38dbebbaff68bb
tong <tong@disktree.net>
2012-11-20 13:09:34 +0100

    string as type for localStorage

--- LocalStorage.hx
+++ LocalStorage.hx
@@ -2,8 +2,8 @@
 @:native("localStorage") extern class LocalStorage {
 	static var length(default,null) : Int;
 	static function key( index : Int ) : String;
-	static function setItem( key : String, value : Dynamic ) : Void;
-	static function getItem( key : String ) : Dynamic;
+	static function setItem( key : String, value : String ) : Void;
+	static function getItem( key : String ) : String;
 	static function removeItem( key : String ) : Void;
 	static function clear() : Void;
 }
